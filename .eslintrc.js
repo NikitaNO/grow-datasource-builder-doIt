c6e9cea452a794952fc25dcddcc95cdfa01ae47d
Derek Clair <derek@derekclair.com>
2017-05-10 11:56:57 -0600

    Get `eslint` running locally

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'mocha': true,
     'jquery': true
   },
+  'parser': 'babel-eslint',
   'parserOptions': {
     'sourceType': 'module',
     'ecmaVersion': 8,
@@ -19,7 +20,7 @@ module.exports = {
   ],
   'extends': [
     'eslint:recommended',
-	'plugin:react/recommended',
+	  'plugin:react/recommended',
     'plugin:lodash/canonical'
   ],
   'globals': {
